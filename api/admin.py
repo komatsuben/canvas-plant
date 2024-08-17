5af28741df058236c22fd95cb01c5ae24fc73802
Aaron Hartono <aaronhartono28@gmail.com>
2024-08-17 20:36:00 +0700

    Add admin sort and action

--- api/admin.py
+++ api/admin.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 from .models import User, Transaction
 
 # Register your models here.
+
+def toggle_is_success(modeladmin, request, queryset):
+    for transaction in queryset:
+        transaction.is_success = not transaction.is_success
+        transaction.save()
+
 class TransactionAdmin(admin.ModelAdmin):
-    # Display all fields in the list view
     list_display = [field.name for field in Transaction._meta.fields]
-    
-    # Make all fields read-only except 'is_success'
     readonly_fields = [field.name for field in Transaction._meta.fields if field.name != 'is_success']
-    
-    # Specify editable fields
     fields = ('is_success',)
+    ordering = ['type']
+    
+    actions = [toggle_is_success]
+    toggle_is_success.short_description = "Toggle is_success"
 
 admin.site.register(User)
 admin.site.register(Transaction, TransactionAdmin)
